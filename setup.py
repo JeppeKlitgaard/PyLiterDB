674109fc7ec9166f95ee8897acffb8ecfc6134aa
dkkline <jeppe@dapj.dk>
2013-04-05 21:59:29 +0200

    PEP8'd and removed unused code from setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,8 @@ from distutils.core import setup
 import sys
 
 package_dir = {
-    'PyDbLite.test':'PyDbLite/test'
-    }
-
-package_data = {
-    'doc':['*.html','*.css','img/*.jpg','fr/*.html','fr/*.css',
-        'en/*.html','en/*.css','en/*.jpg']
-    }
+    'PyDbLite.test': 'PyDbLite/test'
+}
 
 setup(name='PyDbLite',
       version='2.6',
@@ -19,7 +14,7 @@ setup(name='PyDbLite',
       author='Pierre Quentel',
       author_email='pierre.quentel@gmail.com',
       url='http://www.pydblite.net/',
-      packages=['PyDbLite','PyDbLite.test'],
+      packages=['PyDbLite', 'PyDbLite.test'],
       package_dir=package_dir,
       package_data=package_data,
       classifiers=[
@@ -29,6 +24,4 @@ setup(name='PyDbLite',
           'License :: OSI Approved :: BSD License',
           'Programming Language :: Python',
           'Topic :: Database :: Database Engines/Servers'
-          ]
-     )
-
+      ])
